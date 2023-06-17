61089ab7c2fb4ab43e34a51b5d2b0c19277feb60
bluemiaomiao <bluemiaomiao@users.noreply.github.com>
2023-06-17 15:37:16 +0000

    deploy: f456fe8ffd44900a6337e9a69193d8f361f2e29e

--- js/halo.js
+++ js/halo.js
@@ -13,11 +13,11 @@ function main() {
     const currentLangEnvTag = document.getElementById("current-lang-env")
     
     if (lang.startsWith("zh")) {
-        currentLangEnvTag.innerText = "当前浏览器语言环境：" + lang;
+        currentLangEnvTag.innerText = "语言环境：" + lang + ", 本站点由 WecomZ 赞助支持。";
     } else if (lang.startsWith("en")) {
-        currentLangEnvTag.innerText = "Current browser language environment: " + lang;
+        currentLangEnvTag.innerText = "Current language environment: " + lang + ", this site is sponsored and supported by WecomZ.";
     } else {
-        currentLangEnvTag.innerText = "Warning: Can not supported browser language environment, use default: " + lang;
+        currentLangEnvTag.innerText = "Warning: Can not supported browser language environment, use default: " + lang + ", this site is sponsored and supported by WecomZ.";
     }
 }
 
