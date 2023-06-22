f174e0830d56c884b386c35cd2e773d33cb7b77c
bluemiaomiao <bluemiaomiao@users.noreply.github.com>
2023-06-22 14:33:21 +0000

    deploy: 411c96a45e287f1396274c171523f55d06caa612

--- js/halo.js
+++ js/halo.js
@@ -18,41 +18,8 @@ function autoChangeIndexDonors() {
     }
 }
 
-function smartRedirect() {
-    const host = window.location.host;
-    const href = window.location.href;
-    const origin = window.location.origin;
-
-    if (host.startsWith("localhost") || host.startsWith("127.0.0.1")) {
-        console.log("Debug mode, smart redirect is disabled.")
-        return
-    }
-
-    let lang =  "en-US";
-    if (navigator.language) {
-        lang = navigator.language
-    } else {
-        if (navigator.userLanguage) {
-            lang = navigator.userLanguage
-        }
-    }
-
-    if (lang.startsWith("zh")) {
-        if (!host.startsWith("bluemiaomiao.gitee.io")) {
-            const newHref = href.replace(origin, "https://bluemiaomiao.gitee.io")
-            window.location.replace(newHref)
-        }
-    } else {
-        if (!host.startsWith("bluemiaomiao.github.io")) {
-            const newHref = href.replace(origin, "https://bluemiaomiao.github.io")
-            window.location.replace(newHref)
-        }
-    }
-}
-
 function main() {
     autoChangeIndexDonors()
-    smartRedirect()
 }
 
 main()
